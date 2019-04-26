452780dc1147ffbcc3ffc0dbcfd142a2dc448cec
mdamore0 <mdamore8192@gmail.com>
2019-04-26 15:50:46 -0400

    Added basic num function

--- python_learning.py
+++ python_learning.py
@@ -6,4 +6,10 @@ def hello_world():
     str_beg_script = "Hello world"
     print(str_beg_script)
 
+def loop_numbers():
+    lst_rand_numbers = [1,2,3,5]
+    for num in lst_rand_numbers:
+        print(num*10)
+
 hello_world()
+loop_numbers()
